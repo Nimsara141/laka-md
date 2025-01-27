bdc04d1dc725385f8148c5b381e616218f4bf936
Nimsara141 <thimiranimsara697@gmail.com>
2025-01-27 20:22:05 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,9 +7,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 //👉 Put Your Session Id 🐣 👈කියන එක කපලා අයින් කරලා ඔයාගේ Session id එක දාන්න.
-SESSION_ID: process.env.SESSION_ID || "Put Your Session Id 🐣",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=AlwngZAA#1PQleyFw1F7RSA02RdZcPW2Gl_D2xCvqerG8J2Voi4w",
 //👉 Put Your Mongode Url 🖇️ 👈කියන එක කපලා අයින් කරලා ඔයාගේ Mongode Url එක දාන්න.
-MONGODB: process.env.MONGODB || "Put Your Mongode Url 🖇️",
+MONGODB: process.env.MONGODB || "mongodb+srv://thimiranimsara897:thimira2009#@cluster0.q4ndj.mongodb.net/",
 PREFIX: process.env.PREFIX || '.' ,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94781121579' : process.env.OWNER_NUMBER,
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
