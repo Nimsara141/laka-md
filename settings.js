07e60d089c69bbf874b980a670b416701e24120e
Nimsara141 <thimiranimsara697@gmail.com>
2025-01-27 15:03:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 //👉 Put Your Session Id 🐣 👈කියන එක කපලා අයින් කරලා ඔයාගේ Session id එක දාන්න.
-SESSION_ID: process.env.SESSION_ID || "Manu-MD&rZsH0TgT#aBsS0vEjkIfum1_W2R5Dufx40x1XIGQZ_90vh7-D8io",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=AlwngZAA#1PQleyFw1F7RSA02RdZcPW2Gl_D2xCvqerG8J2Voi4w",
 //👉 Put Your Mongode Url 🖇️ 👈කියන එක කපලා අයින් කරලා ඔයාගේ Mongode Url එක දාන්න.
 MONGODB: process.env.MONGODB || "mongodb+srv://thimiranimsara897:thimira2009#@cluster0.q4ndj.mongodb.net/",
 PREFIX: process.env.PREFIX || '.' ,
